3550be2e880a1844477d12b05ca00fa2ebc35c10
Facundo Torterola <torterola.facundo@gmail.com>
2020-09-24 19:06:25 -0300

    create user and add response header tokens

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,21 +1,32 @@
-
+require 'byebug'
 class AuthenticationController< ApplicationController
 
     def login
-        payload = validate_token(params[:token])
-        render json:{payload:payload}, status: :ok
+        payload = GoogleValidationTokenService.validate_token(params[:token])
+        if payload
+          # update token, generate updated auth headers for response
+          user_to_create = payload
+          generate_random_password(user_to_create)
+          
+          user=User.create(email: user_to_create["email"],password: user_to_create[:password],password_confirmation: user_to_create[:password_confirmation])
+
+          new_auth_header = user.create_new_auth_token()
+          # update response with the header that will be required by the next request
+          response.headers.merge!(new_auth_header)
+          render json:{payload:payload}, status: :ok        
+      else
+        render json:{error: 'Invalid Token'} , status: :unauthorized
+      end
     end
     
 
-    def validate_token(token)
-        validator = GoogleIDToken::Validator.new
-        begin
-            @payload = validator.check(token, ENV['GOOGLE_CLIENT_ID'])            
-          rescue GoogleIDToken::ValidationError => e
-            false
-          end
-        
+    private 
+    def generate_random_password(user)
+      password = SecureRandom.urlsafe_base64(nil, false)
+      user[:password] =password
+      user[:password_confirmation]=password
     end
+    
     
     
 end
---
+++ app/services/google_validation_token_service.rb
@@ -0,0 +1,14 @@
+class GoogleValidationTokenService
+    
+
+    def self.validate_token(token)
+        validator = GoogleIDToken::Validator.new
+        begin
+            @payload = validator.check(token, ENV['GOOGLE_CLIENT_ID'])            
+          rescue GoogleIDToken::ValidationError => e
+            false
+          end
+    
+    end
+    
+end
