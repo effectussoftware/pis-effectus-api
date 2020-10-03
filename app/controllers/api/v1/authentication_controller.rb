40c27acb189aabce23d61b18b81de57145524e81
Kalil de Lima <kalil@rootstrap.com>
2020-10-03 13:39:51 -0300

    fix rubocop offenses

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -4,23 +4,23 @@ module Api
   module V1
     class AuthenticationController < Api::V1::ApiController
       def login
-        # Rises UnauthorizedException if token isn't valid
-        user_from_google = GoogleValidationTokenService.validate_token(params[:token])
-
-        # update token, generate updated auth headers for response
-        @user = User.where(email: user_from_google['email'])
-                    .first_or_create!(create_params(user_from_google))
-        if @user["is_active"]
-          new_auth_header = @user.create_new_auth_token
-          # update response with the header that will be required by the next request
-          response.headers.merge!(new_auth_header)
-          response.headers.merge!({ 'uid' => @user.uid })
-        else
-          raise ::UnauthorizedException, 'Invalid User'
-        end
+        setup_google_user
+        new_auth_header = @user.create_new_auth_token
+        # update response with the header that will be required by the next request
+        response.headers.merge!(new_auth_header)
+        response.headers.merge!({ 'uid' => @user.uid })
       end
 
       private
+
+      def setup_google_user
+        # Rises UnauthorizedException if token isn't valid
+        user_from_google = GoogleValidationTokenService.validate_token(params[:token])
+        # update token, generate updated auth headers for response
+        @user = User.where(email: user_from_google['email'])
+                    .first_or_create!(create_params(user_from_google))
+        raise ::UnauthorizedException, 'Invalid User' unless @user[:is_active]
+      end
 
       def create_params(user)
         {
