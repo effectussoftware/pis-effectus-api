f5b68c1a2ce92cb5c664c3445a5579efd92a8489
guzmanmo <guzmanmartinez123@gmail.com>
2020-10-15 17:56:44 -0300

    fix schema problem

--- db/schema.rb
+++ db/schema.rb
@@ -42,26 +42,15 @@ ActiveRecord::Schema.define(version: 2020_10_09_021853) do
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.string "name"
     t.string "picture"
     t.string "email"
-    t.boolean "is_admin"
-    t.boolean "is_active"
+    t.boolean "is_admin", default: false
+    t.boolean "is_active", default: true
     t.json "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
