30d2e471fa41d0447ea1b0ab5f48f0710297dd5c
Kalil de Lima <kalil@rootstrap.com>
2020-10-15 19:09:23 -0300

    add migration to recurrent events

--- app/views/api/v1/admin/communications/_communication.json.jbuilder
+++ app/views/api/v1/admin/communications/_communication.json.jbuilder
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
-json.extract! communication, :id, :title, :text, :published, :created_at, :updated_at
+json.extract! communication,
+              :id,
+              :title,
+              :text,
+              :published,
+              :recurrent_at,
+              :created_at,
+              :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_145605) do
+ActiveRecord::Schema.define(version: 2020_10_15_220149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_09_26_145605) do
     t.boolean "published", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "recurrent_on"
   end
 
   create_table "users", force: :cascade do |t|
