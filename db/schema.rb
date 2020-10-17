b2fe9496cf674411741af77ed73b94de797c3c7c
Christian Deleon <christian.deleon12@gmail.com>
2020-10-17 13:56:16 -0300

    Fix review_action_item model. Added Review jbuilder.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_182729) do
+ActiveRecord::Schema.define(version: 2020_10_17_163958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_10_11_182729) do
     t.bigint "review_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed"
     t.index ["review_id"], name: "index_review_action_items_on_review_id"
     t.index ["user_id"], name: "index_review_action_items_on_user_id"
   end
