60b773f8b643c4bb0c22ab921d3ff07ed96b8eb9
guzmanmo <guzmanmartinez123@gmail.com>
2020-10-22 19:42:47 -0300

    add nested attributes in create and update

--- app/controllers/api/v1/admin/reviews_controller.rb
+++ app/controllers/api/v1/admin/reviews_controller.rb
@@ -14,15 +14,7 @@ module Api
         end
 
         def create
-          ActiveRecord::Base.transaction do
-            @review = Review.create!(review_params.merge(reviewer_id: current_user.id))
-            review_action_items = params[:review_action_items].map do |item|
-              ReviewActionItem.create!(
-                create_review_action_item(item).merge(review_id: @review.id)
-              )
-            end
-            @review.review_action_item = review_action_items
-          end
+          @review = Review.create!(create_review_params.merge(reviewer_id: current_user.id))
           render :show
         end
 
@@ -31,22 +23,24 @@ module Api
         end
 
         def update
-          @review = Review.find(params[:user_id])
-          @review.update!(update_params)
+          @review = Review.find(params[:id])
+          @review.update!(update_review_params)
+          render :show
         end
 
         private
 
-        def update_params
-          params.require(:review).permit(:completed, :review_action_item)
+        def update_review_params
+          params.require(:review)
+                .permit(:description,
+                        review_action_items_attributes: %i[id description completed commitment_owner])
         end
 
-        def create_review_action_item(item)
-          item.permit(:description, :type, :completed)
-        end
-
-        def review_params
-          params.require(:review).permit(:description, :review_action_item, :user_id)
+        def create_review_params
+          params.require(:review)
+                .permit(:description,
+                        :user_id,
+                        review_action_items_attributes: %i[description completed commitment_owner])
         end
       end
     end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,6 @@
 class Review < ApplicationRecord
   belongs_to :reviewer, class_name: 'User'
   belongs_to :user
-  has_many :review_action_item
+  has_many :review_action_items, dependent: :delete_all
+  accepts_nested_attributes_for :review_action_items
 end
