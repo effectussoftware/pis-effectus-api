f4c02cdaee5260a9923db68e4fed1ff69e36581a
Kalil de Lima <kalil@rootstrap.com>
2020-10-25 19:57:27 -0300

    add images to feed

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -4,11 +4,14 @@ class Feed
   attr_reader :id, :type, :text, :title, :updated_at
 
   def self.from_communication(communication)
+    image = communication.image.attached? ? communication.image_url : nil
+
     new(id: communication.id,
         title: communication.title,
         text: communication.text,
         type: 'communication',
-        updated_at: communication.updated_at)
+        updated_at: communication.updated_at,
+        image: image)
   end
 
   def initialize(args)
@@ -17,5 +20,6 @@ class Feed
     @text = args[:text]
     @type = args[:type]
     @updated_at = args[:updated_at]
+    @image = args[:image]
   end
 end
