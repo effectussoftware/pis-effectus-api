e1d9d8eaa04de7beca0103d1981d07f0059f6d50
Christian Deleon <christian.deleon12@gmail.com>
2020-10-28 21:26:10 -0300

    Feat push notification for reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -13,6 +13,23 @@ RSpec.describe Review, type: :model do
         .to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Reviewer the reviewer must be an admin')
     end
   end
+
+  describe 'after creates' do
+    it 'sends notification' do
+      Timecop.freeze(Time.zone.local(2020))
+      user_review = create(:user)
+      allow(user_review).to receive(:send_notification).and_return(true)
+      create(:review)
+      rev = build(:review, user: user_review)
+      expect(user_review).to receive(:send_notification)
+        .with(rev.title,
+              'You have a new review available.',
+              { id: Review.last.id + 1, updated_at: Time.zone.now, type: rev.class.to_s })
+      expect(rev.save).to eq(true)
+      Timecop.return
+    end
+  end
+
   describe 'Associations' do
     it { should belong_to(:user) }
     it { should belong_to(:reviewer) }
