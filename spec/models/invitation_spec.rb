7383997fba17049f3a7401a5c75c6858a0dd16d6
DJA <drod89@gmail.com>
2020-11-10 19:50:56 -0300

    Fix, add tests for Invitation.new_updates_since_last_seen?

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -9,4 +9,27 @@ RSpec.describe Invitation, type: :model do
       should belong_to(:event)
     end
   end
+
+  describe '.new_updates_since_last_seen?' do
+    it "is false if user hasn't seen the invitation" do
+      event = create(:event)
+      invitation = event.invitations.last
+      invitation.update(changed_last_seen: nil)
+      expect(invitation.new_updates_since_last_seen?).to eq(false)
+    end
+
+    it "is false if there hasn't been any changes since last seen" do
+      event = create(:event)
+      invitation = event.invitations.last
+      invitation.update(changed_last_seen: 1.hour.from_now)
+      expect(invitation.new_updates_since_last_seen?).to eq(false)
+    end
+
+    it 'is true if there are new changes seen user last saw the invitation' do
+      event = create(:event)
+      invitation = event.invitations.last
+      invitation.update(changed_last_seen: 1.hour.ago)
+      expect(invitation.new_updates_since_last_seen?).to eq(true)
+    end
+  end
 end
