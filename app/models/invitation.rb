a1487235352cbe01b74d42149789affd5a6a1046
cmaidana <kristian.maidana@gmail.com>
2020-11-12 10:31:46 -0300

    Add send notification when invitation is created

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,8 +3,19 @@
 class Invitation < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  after_create :send_notification
 
   def new_updates_since_last_seen?
     changed_last_seen.present? && event.updated_event_at > changed_last_seen
+  end
+
+  private
+
+  def send_notification
+    user.send_notification(
+      event.name,
+      'You have a new event invitation.',
+      { id: id, updated_at: updated_at, event: event.id, start_time: event.start_time, type: self.class.to_s }
+    )
   end
 end
