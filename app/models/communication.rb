36c0fe0a50aae20614c5b568a593ebba44db2c71
Facundo Torterola <torterola.facundo@gmail.com>
2020-11-14 19:13:59 -0300

    fix rubocop

--- app/models/communication.rb
+++ app/models/communication.rb
@@ -51,9 +51,7 @@ class Communication < ApplicationRecord
   private
 
   def cant_destroy_if_published_not_recurrent
-    # rubocop:disable Layout/LineLength
     throw :abort, "can't delete published non recurrent communications" if published_was && !recurrent_on_was
-    # rubocop:enable Layout/LineLength
   end
 
   def cant_update_if_published
